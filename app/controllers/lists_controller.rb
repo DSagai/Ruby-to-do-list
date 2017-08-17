025f9e8a049d6e06a47614a1fdf93faae7c80c84
DSagai <accn1@yandex.ru>
2017-08-17 20:27:01 +0200

    refactoring of list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,7 @@
 class ListsController < ApplicationController
+before_action :get_list, only: [:show, :edit, :update, :destroy]
+before_action :check_auth, only: [:edit, :update, :destroy]
+
 
   #GET /lists
   def index
@@ -10,6 +13,30 @@ class ListsController < ApplicationController
   end
 
   def show
+    if @list.user == current_user
+      render :edit
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private def get_list
     @list = List.find_by_token(params[:id])
+  end
+
+  private def check_auth
+    if current_user != @list.user
+      render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
+    end
   end
 end
